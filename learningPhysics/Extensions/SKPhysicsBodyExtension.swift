40cdb515c8f3bdb5e1252dd5fa1b2895dea1fec2
Michał Kurkowski <michalkurkowski93@gmail.com>
2018-08-26 22:22:37 +0200

    added ball physics and button

---
+++ learningPhysics/Extensions/SKPhysicsBodyExtension.swift
@@ -0,0 +1,42 @@
+//
+//  extension.swift
+//  learningPhysics
+//
+//  Created by Michał Kurkowski on 25/08/2018.
+//  Copyright © 2018 kura. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+extension SKPhysicsBody {
+  static func brickBody(size: CGSize) -> SKPhysicsBody {
+    let physicsBody = SKPhysicsBody(rectangleOf: size)
+    physicsBody.affectedByGravity = true
+    physicsBody.isDynamic = true
+    physicsBody.mass = 0.8
+    physicsBody.linearDamping = 0.1
+    physicsBody.angularDamping = 0.1
+    physicsBody.collisionBitMask = 0x1
+    physicsBody.categoryBitMask = 0x1
+    physicsBody.friction = 0.3
+    physicsBody.restitution = 0.8
+    return physicsBody
+
+  }
+  
+  static func ballBody(size: CGSize) -> SKPhysicsBody {
+    let physicsBody = SKPhysicsBody(rectangleOf: size)
+    physicsBody.affectedByGravity = true
+    physicsBody.isDynamic = true
+    physicsBody.mass = 0.2
+    physicsBody.friction = 0.5
+    physicsBody.restitution = 1
+    physicsBody.linearDamping = 0.2
+    physicsBody.angularDamping = 0.2
+    physicsBody.collisionBitMask = 0x1
+    physicsBody.categoryBitMask = 0x1
+    return physicsBody
+    
+  }
+}
--- learningPhysics/GameScene.swift
+++ learningPhysics/GameScene.swift
@@ -10,80 +10,62 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+  
+  private var label : SKLabelNode?
+  private var ballLabel : SKLabelNode?
+  private var brick : SKSpriteNode?
+  
+  override func didMove(to view: SKView) {
+    self.label = self.childNode(withName: "//dropLabel") as? SKLabelNode
+    self.ballLabel = self.childNode(withName: "//ballLabel") as? SKLabelNode
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    self.brick = self.childNode(withName: "//brick") as? SKSpriteNode
     
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+    let framePhysics = SKPhysicsBody(edgeLoopFrom: self.frame)
+    framePhysics.friction = 0.2
+    framePhysics.restitution = 0.6
+    self.physicsBody = framePhysics
+  }
+  
+  
+  
+  func touchDown(atPoint pos : CGPoint) {
+  }
+  
+  func touchMoved(toPoint pos : CGPoint) {
+  }
+  
+  func touchUp(atPoint pos : CGPoint) {
+    
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    
+    for t in touches {
+      if self.nodes(at: t.location(in: self)).contains(self.label!) {
+        self.label!.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        self.brick!.physicsBody = SKPhysicsBody.brickBody(size: self.brick!.size)
+      }
+      
+      if self.nodes(at: t.location(in: self)).contains(self.ballLabel!) {
+        let ball = BallWrapper(circleOfRadius: 30).ball
+        self.addChild(ball)
+      }
+      
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+  }
+  
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+  }
+  
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+  }
+  
+  override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+  }
+  
+  
+  override func update(_ currentTime: TimeInterval) {
+    // Called before each frame is rendered
+  }
 }
