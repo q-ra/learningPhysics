a115091c30e1571d3e1cb6b98e576a2aff9eac4c
Michał Kurkowski <michalkurkowski93@gmail.com>
2018-08-26 22:22:59 +0200

    up

---
+++ learningPhysics/Ball.swift
@@ -0,0 +1,28 @@
+//
+//  Ball.swift
+//  learningPhysics
+//
+//  Created by Michał Kurkowski on 26/08/2018.
+//  Copyright © 2018 kura. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+
+
+
+class BallWrapper{
+  
+  var ball: SKShapeNode
+  init(circleOfRadius: CGFloat ){
+    self.ball = SKShapeNode(circleOfRadius: circleOfRadius)
+    self.ball.physicsBody = SKPhysicsBody.ballBody(size: self.ball.frame.size)
+    self.ball.position = CGPoint(x: drand48() * 400 - 200, y: drand48() * 1000)
+    let color = UIColor(red: CGFloat(drand48()), green: CGFloat(drand48()), blue: CGFloat(drand48()), alpha: 1)
+    self.ball.fillColor = color
+    self.ball.strokeColor = color
+  }
+  
+  
+}
